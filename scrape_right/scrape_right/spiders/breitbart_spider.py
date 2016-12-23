85e0486e2383364e9fc04c5eae6dd7b7a54b8445
bstarling <benjamin.starling@gmail.com>
2016-12-23 08:19:07 -0500

    Improve url parsing to reduce duplicates, tweak settings

--- scrape_right/scrape_right/spiders/breitbart_spider.py
+++ scrape_right/scrape_right/spiders/breitbart_spider.py
@@ -15,49 +15,56 @@ class BreitbartSpider(scrapy.Spider):
 
     def parse_url(self, response):
         '''Parse top nav bar for major categories. Schedule requests to each category
-        landing page and call parse_landing_page'''
-
-        for item in response.xpath('//li/a'):
-            category = item.xpath('@href').extract_first()
-            yield scrapy.Request(response.urljoin(category),
-                                 callback=self.parse_landing_page)
-
-    def parse_landing_page(self, response):
-        '''Parse main page looking for articles. Schedule requests article URL
-        and parse with parse_article
+        landing page and call parse_landing_page
         '''
 
-        # parse links on front page
+        # Menu bar found at top of page contains sub-sections of site
+        menu_bar = response.xpath('//ul[contains(@class, "menu")]')[0:2]  # bottom bar is duplicate
+        for menu in menu_bar:
+            category_list = menu.xpath('.//a/@href').extract()
+            for category in category_list:
+                yield scrapy.Request(response.urljoin(category),
+                                     callback=self.parse_landing_page)
+
+    def parse_landing_page(self, response):
+        '''Parse landing page looking for articles. Schedule requests to article URL
+        and submit to parse_article
+        '''
+
+        # search landing page for articles
         for article in response.xpath('//article/a'):
             article_url = article.xpath('@href').extract_first()
             yield scrapy.Request(response.urljoin(article_url),
                                  callback=self.parse_article)
-        # get links from top nav bar
 
     def parse_article(self, response):
-        '''Main function for parsing articles. Takes response from parse_homepage
-        and yields Article item'''
+        '''Main function for parsing articles. Takes article URLS and yields
+        Article item
+        '''
 
         # Limit to main article body to avoid tag conflicts with other portions of page
         page = response.xpath('//div[contains(@id, "MainW")]')
 
         # Helper function to combine lead plus text body into one string
         def get_text_blob(page):
+
+            # this is the lead
             if page.xpath('//h2/text()').extract_first():
                 blob = page.xpath('//h2/text()').extract_first()
+            # no lead
             else:
                 blob = ''
+
             for text in page.xpath('//p/text()'):
                 blob = blob + text.extract()
             return blob
 
-        # Create and return article item
         article = Article(
-            language=response.xpath('/html/@lang').extract(),
+            language=response.xpath('/html/@lang').extract_first(),
             url=response.url,
             authors=page.xpath('//a[contains(@class, "byauthor")]/text()').extract_first(),
-            pub_datetime=page.xpath('//time[contains(@class, "published")]/@datetime').extract(),
-            modified_datetime=page.xpath('//time[contains(@class, "modified")]/@datetime').extract(),
+            pub_datetime=page.xpath('//time[contains(@class, "published")]/@datetime').extract_first(),
+            modified_datetime=page.xpath('//time[contains(@class, "modified")]/@datetime').extract_first(),
             title=page.xpath('//h1[contains(@itemprop, "headline")]/text()').extract_first(),
             lead=page.xpath('//h2/text()').extract_first(),
             text_blob=get_text_blob(page),
