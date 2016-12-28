3b510a7aa389c56791d5acec8d9428bf3e6117a9
bstarling <benjamin.starling@gmail.com>
2016-12-28 08:09:07 -0500

    refactor Breitbart spider

--- scrape_right/scrape_right/spiders/breitbart_spider.py
+++ scrape_right/scrape_right/spiders/breitbart_spider.py
@@ -1,34 +1,41 @@
 import scrapy
 from scrape_right.items import Article
-
+from scrapy import Request
 
 class BreitbartSpider(scrapy.spiders.CrawlSpider):
+    '''Breitbart news spider
+
+    Arguments:
+    limit -- Pagination limit where crawler will stop. There are approximately 30-35
+    articles per page. (Default=0 will only search frontpage). Example: limit=5
+    will search 5 pages of archives for all news categories found on Breitbart
+    front page ~1500 articles
+
+    Example Command: scrapy crawl breitbart -o <outputfile.json> -a limit=5 --logfile <logfile.txt>
+    '''
+
     name = 'breitbart'
 
-    def __init__(self, limit=None, *args, **kwargs):
+    def __init__(self, limit=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.limit = limit
+        self.limit = int(limit)
+
+
 
     def start_requests(self):
+        urls = ['http://www.breitbart.com']
 
-        urls = [
-            'http://www.breitbart.com/'
-        ]
-
-        # limits crawl to main page
-        if self.limit == 'Y':
+        # main landing page only
+        if self.limit == 0:
             for url in urls:
                 yield scrapy.Request(url=url, callback=self.parse_landing_page)
 
         # crawl all subsections
         else:
             for url in urls:
-                yield scrapy.Request(url=url, callback=self.parse_url)
+                yield scrapy.Request(url=url, callback=self.search_categories)
 
-    def parse_url(self, response):
-        '''Parse top nav bar for major categories. Schedule requests to each category
-        landing page and call parse_landing_page
-        '''
+    def search_categories(self, response):
 
         # Menu bar found at top of page contains sub-sections of site
         menu_bar = response.xpath('//ul[contains(@class, "menu")]')[0:2]  # bottom bar is duplicate
@@ -39,15 +46,22 @@ class BreitbartSpider(scrapy.spiders.CrawlSpider):
                                      callback=self.parse_landing_page)
 
     def parse_landing_page(self, response):
-        '''Parse landing page looking for articles. Schedule requests to article URL
-        and submit to parse_article
-        '''
+        # pagination get "older posts" href
+        next_page = response.xpath('//div[@class="pagination"]/div/a/@href').extract_first()
 
-        # search landing page for articles
-        for article in response.xpath('//article/a'):
+        # main section
+        page = response.xpath('//div[contains(@id, "MainW")]')
+
+        for article in page.xpath('//article/a'):
             article_url = article.xpath('@href').extract_first()
-            yield scrapy.Request(response.urljoin(article_url),
-                                 callback=self.parse_article)
+            yield Request(response.urljoin(article_url),
+                          callback=self.parse_article)
+
+        if next_page:
+            if int(next_page.split('/')[-2]) <= self.limit:
+                yield scrapy.Request(response.urljoin(next_page),
+                                     callback=self.parse_landing_page)
+
 
     def parse_article(self, response):
         '''Main function for parsing articles. Takes article URLS and yields
@@ -55,31 +69,31 @@ class BreitbartSpider(scrapy.spiders.CrawlSpider):
         '''
 
         # Limit to main article body to avoid tag conflicts with other portions of page
-        page = response.xpath('//div[contains(@id, "MainW")]')
+        body = response.xpath('//div[contains(@id, "MainW")]')
 
         # Helper function to combine lead plus text body into one string
-        def get_text_blob(page):
+        def get_text_blob(body):
 
             # this is the lead
-            if page.xpath('//h2/text()').extract_first():
-                blob = page.xpath('//h2/text()').extract_first()
+            if body.xpath('//h2/text()').extract_first():
+                blob = body.xpath('//h2/text()').extract_first()
             # no lead
             else:
                 blob = ''
 
-            for text in page.xpath('//p/text()'):
+            for text in body.xpath('//p/text()'):
                 blob = blob + text.extract()
             return blob
 
         article = Article(
             language=response.xpath('/html/@lang').extract_first(),
             url=response.url,
-            authors=page.xpath('//a[contains(@class, "byauthor")]/text()').extract_first(),
-            pub_datetime=page.xpath('//time[contains(@class, "published")]/@datetime').extract_first(),
-            modified_datetime=page.xpath('//time[contains(@class, "modified")]/@datetime').extract_first(),
-            title=page.xpath('//h1[contains(@itemprop, "headline")]/text()').extract_first(),
-            lead=page.xpath('//h2/text()').extract_first(),
-            text_blob=get_text_blob(page),
+            authors=body.xpath('//a[contains(@class, "byauthor")]/text()').extract_first(),
+            pub_datetime=body.xpath('//time[contains(@class, "published")]/@datetime').extract_first(),
+            modified_datetime=body.xpath('//time[contains(@class, "modified")]/@datetime').extract_first(),
+            title=body.xpath('//h1[contains(@itemprop, "headline")]/text()').extract_first(),
+            lead=body.xpath('//h2/text()').extract_first(),
+            text_blob=get_text_blob(body),
             source='breitbart',
 
         )
